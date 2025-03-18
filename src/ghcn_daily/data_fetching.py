e963d0ac1d158671a5799e1164cb41a6a7ff5dd5
Lokeswar Kudumula <lokeswk1@umbc.edu>
2025-03-18 21:54:56 +0000

    update fetching

--- src/ghcn_daily/data_fetching.py
+++ src/ghcn_daily/data_fetching.py
@@ -16,21 +16,32 @@ class DataFetcher:
         return data
 
     @staticmethod
-    def fetch_and_save_to_dataframe(station_ids):
+    def fetch_and_save_to_dataframe(station_ids, chunk_size=1000):
         all_data = []
-        for station_id in tqdm(station_ids, desc="Fetching Data", unit="station", ncols=100):
-            url = f"https://www.ncei.noaa.gov/pub/data/ghcn/daily/all/{station_id}.dly"
-            data = DataFetcher.read_data_from_url(url)
-            all_data.extend(data)
         
+        # Prepare the headers for the DataFrame
         headers = ["ID", "YEAR", "Month", "ELEMENT"]
         for i in range(1, 32):
             headers.extend([f"VALUE{i}", f"MFLAG{i}", f"QFLAG{i}", f"SFLAG{i}"])
+
+        # Fetch data in chunks
+        for i in tqdm(range(0, len(station_ids), chunk_size), desc="Fetching Data", unit="chunk", ncols=100):
+            chunk_station_ids = station_ids[i:i + chunk_size]
+                
+            chunk_data = []
+            for station_id in chunk_station_ids:
+                url = f"https://www.ncei.noaa.gov/pub/data/ghcn/daily/all/{station_id}.dly"
+                data = DataFetcher.read_data_from_url(url)
+                chunk_data.extend(data)
+            
+            # Process chunk data and add to the all_data list
+            for entry in chunk_data:
+                row = [entry["ID"], entry["YEAR"], entry["Month"], entry["ELEMENT"]]
+                row.extend(entry["DATA"])
+                all_data.append(row)
+
+        # Create DataFrame from all the data collected
+        df = pd.DataFrame(all_data, columns=headers)
         
-        df_data = []
-        for entry in all_data:
-            row = [entry["ID"], entry["YEAR"], entry["Month"], entry["ELEMENT"]]
-            row.extend(entry["DATA"])
-            df_data.append(row)
-        
-        return pd.DataFrame(df_data, columns=headers)
+        print(f"Data fetching and saving completed. Data saved to DataFrame")
+        return df
